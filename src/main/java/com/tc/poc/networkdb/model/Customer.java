a1875b24ef9ba7d44690d03019a659827b7a1d20
anjansharmasid <anjansharmasid@gmail.com>
2019-05-14 13:46:42 +0100

    Add time stamp and remove uniques name attribute.
    Add time stamp and remove uniques name attribute.

--- src/main/java/com/tc/poc/networkdb/model/Customer.java
+++ src/main/java/com/tc/poc/networkdb/model/Customer.java
@@ -1,6 +1,10 @@
 package com.tc.poc.networkdb.model;
 
 import javax.persistence.Table;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import javax.persistence.Column;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import javax.persistence.Entity;
@@ -19,16 +23,26 @@ public class Customer {
 	@Column(name="Id", unique=true, updatable=true, nullable=false)
 	private Long Id;
 
-	@Column(name="name", unique=true, updatable=true, nullable=false)
+	@Column(name="name", unique=false, updatable=true, nullable=false)
 	private String name;
 	
+	@Column(name="createTime", unique=false, updatable=true, nullable=false)
+	private String createTime = new SimpleDateFormat("dd/MM/yyyy hh:mm:ss.SSS").format(new Date()).toString();
+	
+    
 	public Customer() {
 	}
 
 	public Customer(String name) {
 		this.name = name;
+		this.createTime = new SimpleDateFormat("dd/MM/yyyy hh:mm:ss.SSS").format(new Date()).toString();
 	}
 
+	public Customer(String name,String createTime) {
+		this.name = name;
+		this.createTime = createTime;
+	}
+	
 	public Long getId() {
 		return Id;
 	}
@@ -45,13 +59,23 @@ public class Customer {
 		this.name = name;
 	}
 
+	public String getCreateTime() {
+		return createTime;
+	}
+
+	public void setCreateTime(String createTime) {
+		if(createTime.isEmpty()) {
+			this.createTime = new SimpleDateFormat("dd/MM/yyyy hh:mm:ss.SSS").format(new Date()).toString();
+		}
+	}
+
+	
 	@Override
 	public String toString() {
-		return "Customer{" +
-                "Id=" + Id +
-                ", name='" + name + '\'' +
-                '}';
+		return "Customer [Id=" + Id + ", name=" + name + ", createTime=" + createTime + "]";
 	}
+
+	
 	
 	
 }
